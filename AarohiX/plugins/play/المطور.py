1f94deeb070a39d5a585fcbee440fbbd71dbc71e
noooortog <168671984+noooortog@users.noreply.github.com>
2024-05-13 22:20:42 +0300

    Update المطور.py

--- AarohiX/plugins/play/المطور.py
+++ AarohiX/plugins/play/المطور.py
@@ -10,7 +10,7 @@ import config
 )
 async def maker(client: Client, message: Message):
     await message.reply_photo(
-        photo="https://te.legra.ph/file/b39f0cd77aa8fcba21b5d.jpg",
+        photo="https://telegra.ph/file/b8d877d44850c58363ad7.jpg",
         caption="• Dev Bot ↦  مـطور سـورس  المـيوزك نـور الـحاكم \n ━━━━━━━━━━━━ \n • Dev ↦Cr SoUrce:𓏺𝙽𝙾𝚄𝚁 . \n • Bio ↦- 𓏺 𝐖𝐡𝐨𝐞𝐯𝐞𝐫 𝐡𝐮𝐦𝐛𝐥𝐞𝐬 #𝐡𝐢𝐦𝐬𝐞𝐥𝐟 𝐭𝐨 𝐠𝐨𝐝 𝐰𝐢𝐥𝐥 𝐛𝐞 #𝐞𝐱𝐚𝐥𝐭𝐞𝐝:{@noordot} 𓏺",
         reply_markup=InlineKeyboardMarkup(
             [
